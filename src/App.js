f6cf2047861dd3fa83811477c2a041c976eafa04
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-05-29 14:49:57 -0300

    Added GameCivilizations and folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import GameCivilizations from './GameCivilizations/GameCivilizations.js';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <GameCivilizations />
+      </div>
+    );
   }
 }
 
---
+++ src/GameCivilizations/GameCivilizations.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const civsStyles = {
+  width: '360px',
+  height: '980px',
+  backgroundColor: '#e5e5e5',
+  marginTop: '15px',
+  marginLeft: '15px',
+  marginBottom: '15px'
+};
+
+const GameCivilizations = () => (
+  <div className="civs-container" style={civsStyles} />
+);
+
+export default GameCivilizations;
