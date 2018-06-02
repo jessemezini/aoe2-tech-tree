65d8d0d03101d151183787d9f69870360113a8e4
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-06-02 20:00:18 -0300

    added some global css

--- src/index.js
+++ src/index.js
@@ -8,13 +8,30 @@ import registerServiceWorker from './registerServiceWorker';
 
 import bg from './bg.jpg';
 
+const fontUrl =
+  "url('https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre:300,400,700')";
+
 injectGlobal`
+  @import ${fontUrl};
+
   body {
+    font-family: 'Frank Ruhl Libre', serif;
+    font-weight: 300;
+    line-height: 1.3;
+    color: #111111;
     background-color: #ebcb9c;
     background-image: url(${bg});
     background-repeat: repeat-x;
     width: 5837px;
     height: 1008px;
+  }
+
+  strong {
+    font-weight: 700;
+  }
+
+  ul {
+    padding-left: 20px;
   }
 `;
 
