d6b813bfca88c8c4ad7a229d4430a4dc66a6bfad
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-09-03 17:56:07 -0300

    added missing icon for ios and windows

--- src/GameCivilizations/Footer.js
+++ src/GameCivilizations/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => (
       Developed by{' '}
       <a href="https://www.voobly.com/profile/view/55725">[EoK]_OrO_</a>
       <br />
-      If you find anything wrong, please let me know.{' '}
+      Anything wrong, please let me know. <br />
       <a href="http://twitter.com/jessemezini">Twitter (DM open)</a> -{' '}
       <a href="http://facebook.com/jessemezini">Facebook</a>
     </p>
